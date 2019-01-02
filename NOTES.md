a66593a04552f73f9145b38acab98c6a6dd90693
Claire Tran <c.tran83@gmail.com>
2019-01-02 16:21:57 +1100

    Adding static rendering

--- NOTES.md
+++ NOTES.md
@@ -28,3 +28,13 @@ console.log src/components/__tests__/List.test.js:12
 When using `shallow` testing, the children will not be rendered in the test
 e.g. `ListItem` component `<li>` elements 
 So therefore cannot check for `<li>` elements in the test
+
+### Mount
+
+If we want to test the entire tree of a component
+
+### Static Rendering
+
+https://airbnb.io/enzyme/docs/api/render.html
+
+Static HTML is returned instead of an instance of what is rendered
--- src/components/__tests__/List.test.js
+++ src/components/__tests__/List.test.js
@@ -1,16 +1,17 @@
-import { mount } from '../../enzyme';
+import { render } from '../../enzyme';
 
 import List from '../List';
+import { wrap } from 'module';
 
 describe('List tests', () => {
   it('renders list-items', () => {
     const items = ['one', 'two', 'three'];
-    const wrapper = mount(<List items={items} />);
+    const wrapper = render(<List items={items} />);
 
-    console.log(wrapper.debug());
+    wrapper.addClass('foo');
 
     // Expect the wrapper object to be defined
-    // (1) Before ListItem using shallow and (2) using mount
+    // (1) Before ListItem using shallow and (2) using mount and (3) static rendering
     expect(wrapper.find('.list-items')).toBeDefined();
     expect(wrapper.find('.item')).toHaveLength(items.length);
 
@@ -21,17 +22,23 @@ describe('List tests', () => {
 
   it('renders a list item', () => {
     const items = ['Thor', 'Loki'];
-    const wrapper = mount(<List items={items} />);
+    const wrapper = render(<List items={items} />);
 
     // Check if an element in the Component exists
-    expect(wrapper.contains(<li key='Thor' className="item">Thor</li >)).toBeTruthy();
+    //expect(wrapper.contains(<li key='Thor' className="item">Thor</li >)).toBeTruthy();
+
+    // With static rendering
+    expect(wrapper.find('[key=Thor]')).toBeTruthy();
   });
 
   it('renders correct text in item', () => {
     const items = ['John', 'James', 'Luke'];
-    const wrapper = mount(<List items={items} />);
+    const wrapper = render(<List items={items} />);
 
-    //Expect the child of the first item to be an array
-    expect(wrapper.find('.item').get(0).props.children).toEqual('John');
+    //Before: (1) shallow without ListItem and (2) mount Expect the child of the first item to be an array
+    //expect(wrapper.find('.item').get(0).props.children).toEqual('John');
+
+    // After: static rendering 
+    expect(wrapper.find('.item').get(0).children[0].data).toEqual('John');
   });
 });
